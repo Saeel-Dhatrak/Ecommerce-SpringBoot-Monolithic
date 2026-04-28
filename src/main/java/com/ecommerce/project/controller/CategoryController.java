8f3d6d389333f753b6418472e5b83c5ca771d3fc
saeel.dhatrak <saeeldhatrak007@gmail.com>
2026-04-28 23:36:48 +0530

    Created the Category model, controller, service with getAllCategories, createCategory and deleteCategoryById api's

---
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -0,0 +1,35 @@
+package com.ecommerce.project.controller;
+
+import com.ecommerce.project.model.Category;
+import com.ecommerce.project.service.CategoryService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/api/public/categories")
+    public List<Category> getAllCategories(){
+        return categoryService.getAllCategories();
+    }
+
+    @PostMapping("/api/public/categories")
+    public String createCategory(@RequestBody Category category){
+        categoryService.createCategory(category);
+        return "Category Created Successfully";
+    }
+
+    @DeleteMapping("/api/admin/category/{categoryId}")
+    public String deleteCategory(@PathVariable Long categoryId){
+       String status = categoryService.deleteCategory(categoryId);
+       return status;
+    }
+}
