749d2267a68ebd12b93b5716627cc61d78089e31
saeel.dhatrak <saeeldhatrak007@gmail.com>
2026-05-29 08:36:51 +0530

    Add Product agaist a particular Id

--- src/main/java/com/ecommerce/project/controller/ProductController.java
+++ src/main/java/com/ecommerce/project/controller/ProductController.java
@@ -1,9 +1,28 @@
 package com.ecommerce.project.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ecommerce.project.model.Product;
+import com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class ProductController {
+
+    private final ProductService productService;
+
+    ProductController(ProductService productService){
+        this.productService = productService;
+    }
+
+    @PostMapping("/admin/categories/{categoryId}/product")
+    public ResponseEntity<ProductDTO> addProduct(@RequestBody Product product,
+                                                 @PathVariable Long categoryId){
+        ProductDTO productDTO = productService.addProduct(product, categoryId);
+        return new ResponseEntity<>(productDTO, HttpStatus.CREATED);
+    }
+
 }
